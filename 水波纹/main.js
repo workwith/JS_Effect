b220650b924ab81dd731f16ef6652a2775f24c09
lvdianzhao <lv_workwith@163.com>
2020-04-03 00:58:58 +0800

    小球放大缩小

---
+++ 水波纹/main.js
@@ -0,0 +1,48 @@
+/*
+ * @Description:
+ * @Autor: 吕殿朝
+ * @Date: 2020-03-30 22:32:36
+ * @LastEditors: 吕殿朝
+ * @LastEditTime: 2020-03-30 23:23:42
+ */
+const container = document.getElementById("container");
+const circlesArr = [];
+
+let rows = 15;
+let cols = 15;
+
+for (let i = 0; i < cols; i++) {
+  circlesArr[i] = [];
+  for (let j = 0; j < rows; j++) {
+    const circle = document.createElement("div");
+    circle.classList.add("circle");
+    container.appendChild(circle);
+    circlesArr[i].push(circle);
+  }
+}
+// console.log(circlesArr);
+
+circlesArr.forEach((cols, i) => {
+  cols.forEach((circle, j) => {
+    circle.addEventListener("mouseente", () => {
+      growCircles(i, j);
+    });
+  });
+});
+
+function growCircles(i, j) {
+  if (circlesArr[i] && circlesArr[i][j]) {
+    if (!circlesArr[i][j].classList.contains("grow")) {
+      circlesArr[i][j].classList.add("grow");
+      setTimeout(() => {
+        growCircles(i - 1, j);
+        growCircles(i + 1, j);
+        growCircles(i, j - 1);
+        growCircles(i, j + 1);
+      }, 100);
+      setTimeout(() => {
+        circlesArr[i][j].classList.remove("grow");
+      }, 300);
+    }
+  }
+}
